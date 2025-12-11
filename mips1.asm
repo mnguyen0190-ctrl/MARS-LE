394e80e21e440a9c893b50891c9708eb03467b8f
MatthewN. <mnguyen0190@sdsu.edu>
2025-12-11 05:20:48 -0800

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -7,4 +7,23 @@ kai $t1, $t2
 loop:
 pow $t1, -500
 tp message
-it $t1, loop
+it $t1, loop.globl main
+
+main:
+
+    fuel $t0,0          # $t0 = active tire
+    fuel $t3,999        # $t3 = spare tire
+    fuel $t1,0          # $t1 = safe wear level
+
+HAZARD_LOOP:
+    lup  $t0            # tire wear++
+
+    hz   $t0,$t1,PITSTOP   # branch if not equal
+
+    cklap $t0,$t0,HAZARD_LOOP   # unconditional loop
+
+PITSTOP:
+    pit $t0,$t3         # swap registers
+
+END:
+    cklap $t0,$t0,END   # loop forever
