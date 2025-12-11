7b0e76306b2611733abd5564f4119bdb26aa510f
Matthew Nguyen <mnguyen0190@sdsu.edu>
2025-12-11 05:22:06 -0800

    Update

--- mips1.asm
+++ mips1.asm
@@ -1,13 +1,5 @@
-.data
-message: .asciiz "Power level drained by 500!!!\n"
 .text
-pow $t1, 8001
-pow $t2, 4
-kai $t1, $t2
-loop:
-pow $t1, -500
-tp message
-it $t1, loop.globl main
+.globl main
 
 main:
 
